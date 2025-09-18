fa9193598cecf986c1254ab3d5c2c9946f930b5a
Aman <prasadaman671@gmail.com>
2025-09-18 11:36:23 +0530

    hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -27,7 +27,12 @@ def evaluate_model(X_train, y_train, X_test, y_test, models, param):
         
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
+            gs= GridSearchCV(model,para, cv=3, n_jobs=1, verbose=2, refit=True)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
